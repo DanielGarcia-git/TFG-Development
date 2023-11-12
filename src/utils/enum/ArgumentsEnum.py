58ea446fa000432d8e2d0da0f102dcac3502f93e
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2023-11-12 18:57:16 +0100

    Defining arguments

--- src/utils/enum/ArgumentsEnum.py
+++ src/utils/enum/ArgumentsEnum.py
@@ -4,23 +4,26 @@ class Arguments(Enum):
     """_summary_
     """
 
-    REPOSITORY_SETUP = ("-r", "--repository-setup", "Descarga los repositorios especificados en el fichero de repositorios.txt")
-    COMPILER_SETUP = ("-c", "--compiler-setup", "Compila todos los archivos .c que se encuentren en el directorio de repositorios")
-    DATASET_SETUP = ("-d", "--dataset-setup", "Genera el dataset a partir de los archivos .asm generados por el compilador")
-    VERSION = ("-v", "--version", "Muestra la versión del programa")
-    HELP = ("-h", "--help", "Muestra este mensaje de ayuda")
+    REPOSITORY_SETUP = ("-r", "--repository-setup", "Descarga los repositorios especificados en el fichero de repositorios.txt", False)
+    COMPILER_SETUP = ("-c", "--compiler-setup", "Compila todos los archivos .c que se encuentren en el directorio de repositorios", False)
+    LEVEL_SETUP = ("-l", "--level-setup", "Especifica el nivel de optimización del compilador", True)
+    DATASET_SETUP = ("-d", "--dataset-setup", "Genera el dataset a partir de los archivos .asm generados por el compilador", False)
+    VERSION = ("-v", "--version", "Muestra la versión del programa", False)
+    HELP = ("-h", "--help", "Muestra este mensaje de ayuda", False)
 
-    def __init__(self, arg_short: str, arg_large: str, description: str) -> None:
+    def __init__(self, arg_short: str, arg_large: str, description: str, has_arg: bool) -> None:
         """_summary_
 
         Args:
             arg_short (str): _description_
             arg_large (str): _description_
             description (str): _description_
+            has_arg (bool): _description_
         """
 
         self.__arg_short = arg_short
         self.__arg_large = arg_large
+        self.__has_arg = has_arg
         self.__description = description
 
     def __str__(self) -> str:
@@ -29,8 +32,11 @@ class Arguments(Enum):
         Returns:
             str: _description_
         """
-    
-        return f"{self.__arg_short}/{self.__arg_large} - {self.__description}"
+
+        options = ""
+        if self.__has_arg:
+            options = " <arg>"
+        return f"{self.__arg_short}/{self.__arg_large}{options} - {self.__description}"
     
     def getArgShort(self) -> str:
         """_summary_
