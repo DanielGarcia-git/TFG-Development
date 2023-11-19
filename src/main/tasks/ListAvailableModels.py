a0fa2b950bf7589795123fa40d503227ca7a9b53
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2023-11-19 12:19:18 +0100

    Finsih ListAvailableModelsTask

--- src/main/tasks/ListAvailableModels.py
+++ src/main/tasks/ListAvailableModels.py
@@ -1,6 +1,14 @@
+import subprocess
 from main.tasks.Default import DefaultTask
+from utils.enum.PathsEnum import Paths
 
 class ListAvailableModelsTask(DefaultTask):
 
     def defineTask(self) -> None:
-        pass
+
+        command = f"python {Paths.ROOT_PATH_LOCAL_IA_REPOSITORIES}\\lit-gpt\\scripts\\download.py"
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        if result.returncode == 0:
+            print("Listado de modelos disponibles:")
+            print(result.stdout)
