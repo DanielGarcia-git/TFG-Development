c4324e21a3d3641622d9df78b9c32d2d1548754c
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2023-12-16 19:43:53 +0100

    Update CleanUp.py

--- src/main/tasks/CleanUp.py
+++ src/main/tasks/CleanUp.py
@@ -13,6 +13,7 @@ class CleanUpTask(DefaultTask):
         self.logManager.log(f"Eliminando carpeta output: {str(Paths.PATH_TO_OUTPUT.value)}")
         try:
             shutil.rmtree(str(Paths.PATH_TO_OUTPUT.value))
+            shutil.rmtree(str(Paths.PATH_TO_LOG_DIR.value))
         except OSError as e:
             self.logManager.logError(f"Error: {str(e)} - {str(Paths.PATH_TO_OUTPUT.value)}")
 
