133cc3f9d9bd7cab190120567ad9eeca18c5a73a
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2024-01-03 22:14:36 +0100

    Update Version.py

--- src/main/tasks/Version.py
+++ src/main/tasks/Version.py
@@ -2,12 +2,18 @@ from utils.abstract.Default import DefaultTask
 from main.command.ConfigurationClass import Configuration
 
 class VersionTask(DefaultTask):
+    """ This class implements the task that print the version message.
+    """
 
     def defineTask(self) -> None:
+        """This function defines the behaivor of the task, in this case
+           prints the version message.
+        """
+        
         self.__printVersionMessage()
     
     def __printVersionMessage(self) -> None:
-        """_summary_
+        """This function prints the version message
         """
         
         config = Configuration()
