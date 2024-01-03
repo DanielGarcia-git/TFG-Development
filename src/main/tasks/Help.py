14f813046f43c9bbe2b9079b6e53b331557506b4
Daniel García Estévez <daniel.garcia.estevez@hotmail.com>
2024-01-03 22:12:58 +0100

    Update Help.py

--- src/main/tasks/Help.py
+++ src/main/tasks/Help.py
@@ -2,12 +2,18 @@ from utils.enum.ArgumentsEnum import Arguments
 from utils.abstract.Default import DefaultTask
 
 class HelpTask(DefaultTask):
+    """ This class implements the task that print the help message.
+    """
 
     def defineTask(self) -> None:
+        """This function defines the behaivor of the task, in this case
+           prints the help message.
+        """
+
         self.__printHelpMessage()
     
     def __printHelpMessage(self) -> None:
-        """_summary_
+        """This function prints the help message
         """
         
         print("Uso: python main.py [opciones]")
